3ec96dc73c30460aa19d236f6916fd193ee2638c
Animesh Sinha <animeshsinha1903@gmail.com>
2024-09-17 06:02:30 +0530

    added cors

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 dbConfig();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://news-aggregator-frontend-sigma.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
